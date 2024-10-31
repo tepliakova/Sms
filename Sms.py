92100dce8b82c1e82ef3e30e3689bbe78414be2e
Proba <teplyakovateplyakova94@gmail.com>
2024-10-31 23:15:54 +1000

    Исправлен баг с выводом результата в консоль. Теперь информация выводится в консоль

--- Sms.py
+++ Sms.py
@@ -21,10 +21,9 @@ def send_email():
         server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
         server.login(sender_email, password)
         server.send_message(msg)
-        print('Письмо отправлено')
+        result_label.config(text='Письмо отправлено')
     except Exception as e:
-        print(f'Ошибка: {e}')
-
+        result_label.config(text=f'Ошибка: {e}')
     finally:
         if server:
             server.quit()
@@ -33,29 +32,29 @@ window = Tk()
 window.title("Отправка Email")
 window.geometry('500x300')
 
-Label(text="Отправитель:").grid(row=0, column=0, sticky=w)
+Label(text="Отправитель:").grid(row=0, column=0, sticky=W)
 sender_email_entry = Entry()
-sender_email_entry.grid(row=0, column=1, sticky=w)
+sender_email_entry.grid(row=0, column=1, sticky=W)
 
-Label(text="Получатель:").grid(row=1, column=0, sticky=w)
+Label(text="Получатель:").grid(row=1, column=0, sticky=W)
 recipient_email_entry = Entry()
-recipient_email_entry.grid(row=1, column=1, sticky=w)
+recipient_email_entry.grid(row=1, column=1, sticky=W)
 
-Label(text="Пароль приложения:").grid(row=2, column=0, sticky=w)
+Label(text="Пароль приложения:").grid(row=2, column=0, sticky=W)
 password_entry = Entry()
-password_entry.grid(row=2, column=1, sticky=w)
+password_entry.grid(row=2, column=1, sticky=W)
 
-Label(text="Тема письма:").grid(row=3, column=0, sticky=w)
+Label(text="Тема письма:").grid(row=3, column=0, sticky=W)
 subject_entry = Entry()
-subject.grid(row=3, column=1, sticky=w)
+subject_entry.grid(row=3, column=1, sticky=W)
 
-Label(text="Сообщение:").grid(row=4, column=0, sticky=w)
+Label(text="Сообщение:").grid(row=4, column=0, sticky=W)
 body_text = Text(width=45, height=10)
-body_text.grid(row=4, column=1, sticky=w)
+body_text.grid(row=4, column=1, sticky=W)
 
-Button(text="Отправить письмо", command = send_email).grid(row=5, column=1, sticky=w)
+Button(text="Отправить письмо", command = send_email).grid(row=5, column=1, sticky=W)
 
 result_label = Label(text="")
-result_label.grid(row=6, column=1, sticky=w)
+result_label.grid(row=6, column=1, sticky=W)
 
 window.mainloop()
